160621c06e26787908f6d5d4e0a13c089801940b
Darth Degen <darthdegen@gmail.com>
2023-05-02 11:10:54 -0700

    adding Chaos in Color edition

--- src/constants/editions.ts
+++ src/constants/editions.ts
@@ -3,11 +3,11 @@ import { Edition } from "@types";
 export const editions: Edition[] = [
   {
     id: 0, 
-    image_path:"/images/editions/x.png",
-    name:"X",
-    description: "xxxxxxxxx xxxxxxxxx xxxxxxxxx xxxxxxxxx xxxxxxxxx xxxxxxxxx xxxxxxxxx xxxxxxxxx",
-    twitter_url:"https://twitter.com/",
-    exchange_art_url:"https://exchange.art/series/RULE%20BREAKERS/nfts"   
+    image_path:"/images/editions/chaos-in-color.png",
+    name:"Chaos, in color",
+    description: "steal cars. break shit. take drugs. have fun.",
+    twitter_url:"https://twitter.com/rulebreakers___",
+    exchange_art_url:"https://exchange.art/editions/BgK96ensmRrLji3MfP1nmhukt9u88FmFjhN92uCi6Vgm/activity"   
   },
   {
     id: 1,
